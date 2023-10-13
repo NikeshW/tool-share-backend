9eb1c4f9208ffdb9b2abd5f6238c84edc81c535c
NikeshW <nikeshwankhade@pursuit.org>
2023-10-13 16:29:48 -0400

    [KAN-1] this is just a test

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
+const usersController = require('./controllers/UserController')
+
+const toolsController = require('./controllers/ToolsController')
+
+app.use('/users', usersController)
+app.use('/tools', toolsController)
 
 app.get("/", (request, response) => {
-  response.send("Hello world!");
+  response.send("Welcome to Tool Share!");
 });
 
 app.get("/test", (request, response) => {
